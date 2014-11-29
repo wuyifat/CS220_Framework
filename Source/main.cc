424fa5132395652a06f14b4ed31df70ab5a2e799
Shuheng Li <sli037@ucr.edu>
2014-11-28 20:25:48 -0800

    map

--- Source/main.cc
+++ Source/main.cc
@@ -1,6 +1,8 @@
 
 #include "../Headers/grid.h"
+#include "../Headers/edge.h"
 #include "../Headers/problem_object.h"
+#include "../Headers/node.h"
 #include <time.h>
 #include <cstdlib>
 #include <iostream>
@@ -8,7 +10,7 @@
 using std::cerr;
 using std::cout;
 using std::endl;
-
+using std::vector;
 int main(int argc,char* argv[]) {
 
 	// DO NOT CHANGE THIS SECTION OF CODE
@@ -35,6 +37,85 @@ int main(int argc,char* argv[]) {
 	*/
 
 	//Note, we create random paths just as an example of how to create paths, netlists are created similarly
+	
+
+
+
+//**************************Map Blockers******************************
+	vector<Blocker> blocker;
+	int x,y;//blocker location
+	blocker = first_problem->get_blockers();//get blockdrs number
+	for ( int blocker_num = 0; blocker_num < blocker.size(); blocker_num++){
+		x = blocker.at(blocker_num).location.x;
+		y = blocker.at(blocker_num).location.y;
+		if(x < 0 && x>g.get_width()){//check x boundary
+			cout << "x coord is not valid" << endl; }
+		if( y < 0 && y > g.get_height()){//check y boundary
+			cout << "x coord is not valid" << endl; 
+		}
+		for( int i = 0; i < blocker.at(blocker_num).height;i++ ){//blocker height point
+			x = blocker.at(blocker_num).location.x;
+			for( int j = 0; j < blocker.at(blocker_num).width;j++ ){//blocker width point
+				g.get_node(x,y)->set_cost(-1);//set source 's cost is -10
+/*				Node* new_node = new Node(x,y,-1);//if the node is blocker, the cost is -1
+				g.replace_node(new_node);//replace the node
+//Now we need to add connetions to the new Node
+				if (x > 0 && x < g.get_width()){
+					Utilities::Edge* left = new Utilities::Edge(new_node,g.get_node(x-1,y));
+					new_node->add_connection(left);
+				}
+//debug				cout <<"after add :" << g.get_node(x-1,y)->connections_size()<< endl;
+				if (y > 0 && y < g.get_height()){
+					Utilities::Edge* up = new Utilities::Edge(new_node,g.get_node(x,y-1));
+					new_node->add_connection(up);
+				}	
+
+				if(i+1 == blocker.at(blocker_num).height && i+1 < g.get_height()){//woking on the last row of blockers
+					Utilities::Edge* boundary_up = new Utilities::Edge(g.get_node(x,y+1),g.get_node(x,y));
+					g.get_node(x,y+1)->add_connection(boundary_up);
+				}
+*/				x++;
+			}
+/*				if(x < g.get_width()){//woking on last colunm of blockers
+					Utilities::Edge* boundary_left = new Utilities::Edge(g.get_node(x,y),g.get_node(x-1,y));
+					g.get_node(x,y)->add_connection(boundary_left);
+				}
+*/			y++;
+		}
+	}
+
+			cout << "(0,0) is cost is " <<g.get_node(0,0)->get_cost() << " the connection size is " << g.get_node(0,0)->connections_size() << endl;
+
+			cout << "(0,1) is cost is " <<g.get_node(0,1)->get_cost() << " the connection size is " << g.get_node(0,1)->connections_size() << endl;
+			cout << "(1,0) is cost is " <<g.get_node(1,0)->get_cost() << " the connection size is " << g.get_node(1,0)->connections_size() << endl;
+			cout << "(1,1) is cost is " <<g.get_node(1,1)->get_cost() << " the connection size is " << g.get_node(1,1)->connections_size() << endl;
+//**************************Map Connections******************************
+	vector<Connection> connection = first_problem->get_connections();
+	for(int i = 0; i < connection.size();i++){
+		if(connection.at(i).source == connection.at(i).sink){
+			cout << connection.at(i).name << " has a source and sink in same position. Please change the position" << endl;
+		}
+		if(g.get_node(connection.at(i).source)->get_cost() != 0){
+			cout << connection.at(i).name << " has an invalid source position" << endl;
+		}
+		if(g.get_node(connection.at(i).sink)->get_cost() != 0){
+			cout << connection.at(i).name << " has an invalid sink position" << endl;
+		}
+		
+		else{
+		g.get_node(connection.at(i).source)->set_cost(-10);//set source 's cost is -10
+		g.get_node(connection.at(i).sink)->set_cost(-20);//set sink's cost is -20
+		}
+	}
+
+
+			cout << "(0,0) is cost is " <<g.get_node(0,0)->get_cost() << " the connection size is " << g.get_node(0,0)->connections_size() << endl;
+
+
+			cout << "(250,0) is cost is " <<g.get_node(250,0)->get_cost() << " the connection size is " << g.get_node(0,0)->connections_size() << endl;
+			cout << "(250,499) is cost is " <<g.get_node(250,499)->get_cost() << " the connection size is " << g.get_node(0,0)->connections_size() << endl;
+
+			cout << "(250,500) is cost is " <<g.get_node(250,500)->get_cost() << " the connection size is " << g.get_node(0,0)->connections_size() << endl;
 	vector<Path*> paths;
 	srand(time(NULL));
 	int number_paths = first_problem->get_connections().size();
@@ -55,7 +136,11 @@ int main(int argc,char* argv[]) {
 		paths.push_back(new_path);
 	}
 	cout << "Completed." << endl;
+	cout << "add path:" << endl;
 
+
+	g.set_paths(paths);
+//	g.get_path(0);
 	//Print the paths/netlists that you have return from your algorithm
 	for (unsigned i = 0;i < paths.size();i++) {
 		cout << "\tPath " << i+1 << " of " << paths.size() << ": (" 
